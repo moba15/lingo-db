185ee4fba404d4fd3c6788baeb5ace85c7b132be
Moritz Bachmaier <msb@bachmaiers.de>
2025-04-24 16:01:35 +0200

    added groupby node and functionexpression node

---
+++ include/lingodb/compiler/frontend/sql-parser/group_by_node.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "parsed_expression.h"
+#include <memory>
+#include <set>
+#include <string>
+#include <vector>
+namespace lingodb::ast {
+using GroupingSet = std::set<int>;
+class GroupByNode {
+   public:
+   //! The total set of all group expressions
+   std::vector<std::shared_ptr<ParsedExpression>> group_expressions;
+
+   //! The different grouping sets as they map to the group expressions
+   std::vector<GroupingSet> grouping_sets;
+
+   std::string toAsciiAST(uint32_t depth );
+};
+}
