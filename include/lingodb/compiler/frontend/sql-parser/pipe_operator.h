58a9184c84d3737d1c1e6fd9a110abeea8f807ab
Moritz Bachmaier <msb@bachmaiers.de>
2025-04-29 10:57:56 +0200

    change content of pipeop to be a generic astnode. So it can be now any type of node

---
+++ include/lingodb/compiler/frontend/sql-parser/pipe_operator.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "parsed_expression.h"
+#include "tableref.h"
+
+#include <memory>
+namespace lingodb::ast {
+enum class PipeOperatorType : uint8_t {
+   EXPRESSION = 1,
+   TABLE_REF = 2,
+};
+class PipeOperator : public AstNode {
+   public:
+   PipeOperator( std::shared_ptr<AstNode> node);
+   PipeOperatorType type;
+   std::shared_ptr<AstNode> node;
+   std::shared_ptr<PipeOperator> next;
+
+   bool isLast();
+
+   std::string toAsciiAST(uint32_t depth );
+};
+} // namespace lingodb::ast
--- src/compiler/frontend/sql-parser/group_by_node.cpp
+++ src/compiler/frontend/sql-parser/group_by_node.cpp
@@ -1,6 +1,4 @@
-#pragma once
 #include "lingodb/compiler/frontend/sql-parser/group_by_node.h"
-
 #include "lingodb/compiler/frontend/sql-parser/query_node.h"
 namespace lingodb::ast {
 std::string GroupByNode::toAsciiAST(uint32_t depth) {
