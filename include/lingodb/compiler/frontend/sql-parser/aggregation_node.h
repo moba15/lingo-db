6a8205c7a112794f2ad5aa968b2c4c0ec806ad05
Moritz Bachmaier <msb@bachmaiers.de>
2025-04-30 10:00:11 +0200

    added AGGREGATE node

---
+++ include/lingodb/compiler/frontend/sql-parser/aggregation_node.h
@@ -0,0 +1,17 @@
+#include "ast_node.h"
+#include "group_by_node.h"
+#include "parsed_expression/function_expression.h"
+namespace lingodb::ast {
+class AggregationNode : public AstNode {
+   public:
+   AggregationNode();
+
+   std::shared_ptr<GroupByNode> groupByNode;
+   std::vector<std::shared_ptr<FunctionExpression>> aggregations;
+   //TODO having clause
+
+
+   std::string toAsciiAST(uint32_t depth ) override;
+   std::string toDotGraph(uint32_t depth) override;
+};
+} // namespace lingodb::ast
