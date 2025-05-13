c5fac0631bf084e5836a65e212ab772fc41279ea
Moritz Bachmaier <msb@bachmaiers.de>
2025-05-13 16:56:32 +0200

    added pipeoperator type

--- src/compiler/frontend/sql-parser/pipe_operator.cpp
+++ src/compiler/frontend/sql-parser/pipe_operator.cpp
@@ -2,7 +2,7 @@
 
 #include "lingodb/compiler/frontend/sql-parser/query_node.h"
 namespace lingodb::ast {
-PipeOperator::PipeOperator(std::shared_ptr<AstNode> node) : AstNode(NodeType::PIPE_OP), node(node) {
+PipeOperator::PipeOperator(PipeOperatorType type, std::shared_ptr<AstNode> node) : AstNode(NodeType::PIPE_OP), node(node), type(type) {
 }
 bool PipeOperator::isLast() {
    return next == nullptr;
