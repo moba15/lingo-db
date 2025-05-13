bc0f345776289b5654ced123be9dd6398b4a6eda
Moritz Bachmaier <msb@bachmaiers.de>
2025-05-13 16:56:32 +0200

    added pipeoperator type

--- include/lingodb/compiler/frontend/sql-parser/pipe_operator.h
+++ include/lingodb/compiler/frontend/sql-parser/pipe_operator.h
@@ -5,12 +5,22 @@
 #include <memory>
 namespace lingodb::ast {
 enum class PipeOperatorType : uint8_t {
-   EXPRESSION = 1,
-   TABLE_REF = 2,
+   WHERE = 1,
+   SELECT = 2,
+   JOIN = 3,
+   GROUP_BY = 4,
+   ORDER_BY = 5,
+   LIMIT = 6,
+   UNION = 7,
+   INTERSECT = 8,
+   EXCEPT = 9,
+   FROM = 10,
+   AGGREGATE = 11,
+
 };
 class PipeOperator : public AstNode {
    public:
-   PipeOperator(std::shared_ptr<AstNode> node);
+   PipeOperator(PipeOperatorType type, std::shared_ptr<AstNode> node);
    PipeOperatorType type;
    std::shared_ptr<AstNode> node;
    std::shared_ptr<PipeOperator> next;
