dbc86fab26ccd6e0b57fbe9a6feabc2cac059946
Moritz Bachmaier <msb@bachmaiers.de>
2025-05-14 16:09:45 +0200

    analyzer now analyzes simple group by clauses and aggregation function

--- include/lingodb/compiler/frontend/sql-parser/common/column_semantic.h
+++ include/lingodb/compiler/frontend/sql-parser/common/column_semantic.h
@@ -4,14 +4,27 @@
 #include <string>
 #include <vector>
 namespace lingodb::ast {
-struct ColumnInfo {
+enum class NamedResultType : uint8_t {
+   Column = 1,
+   Function = 2
+};
+struct NamedResult {
+   NamedResultType type;
    std::string scope;
+   NamedResult(NamedResultType type, std::string scope) : type(type), scope(scope) {}
+};
+struct FunctionInfo : public NamedResult {
+   std::string name;
+   FunctionInfo(std::string scope, std::string name) : NamedResult(NamedResultType::Function, scope), name(name) {}
+};
+struct ColumnInfo : public  NamedResult {
    catalog::Column column;
+   ColumnInfo(std::string scope, catalog::Column column) : NamedResult(NamedResultType::Column, scope), column(column) {}
 };
 struct TargetInfo {
-   std::vector<std::pair<std::string, ColumnInfo>> namedResults;
-   void map(std::string name, const ColumnInfo cInfo) {
-      namedResults.push_back({name, cInfo});
+   std::vector<std::pair<std::string, std::shared_ptr<NamedResult>>> namedResults;
+   void map(std::string name, std::shared_ptr<NamedResult> cInfo) {
+      namedResults.push_back({name, std::move(cInfo)});
    }
 };
-}
+} // namespace lingodb::ast
