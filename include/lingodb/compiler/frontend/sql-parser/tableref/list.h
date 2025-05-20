449bccba832aa177a36c37fa4fe82c7b5b5bd7f1
Moritz Bachmaier <msb@bachmaiers.de>
2025-05-20 11:12:57 +0200

    moved all TableReferences files into table_ref

--- include/lingodb/compiler/frontend/sql-parser/tableref/list.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "lingodb/compiler/frontend/sql-parser/tableref/basetableref.h"
-#include "lingodb/compiler/frontend/sql-parser/tableref/joinref.h"
-#include "lingodb/compiler/frontend/sql-parser/tableref/subqueryref.h"
-#include "lingodb/compiler/frontend/sql-parser/tableref/tabledescription.h"
