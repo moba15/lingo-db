4a6bc541c71e6f599fb4be65c02ab41f4682a418
Moritz Bachmaier <msb@bachmaiers.de>
2025-05-26 14:11:56 +0200

    Sql_analyzer: rewrote complete analyzing and transformation logic. Analyzer now takes advantage of the pipe like ast structure.
    It takes a parsed AST node and returns a bound AST node

--- include/lingodb/compiler/frontend/analyzer/sql_analyzer.h
+++ include/lingodb/compiler/frontend/analyzer/sql_analyzer.h
@@ -1,68 +1,52 @@
 #pragma once
+#include "bound/bound_expression.h"
+#include "bound/bound_resultmodifier.h"
 #include "lingodb/compiler/frontend/sql-parser/driver.h"
 #include "lingodb/compiler/frontend/sql-parser/query_node.h"
 #include "sql_context.h"
 
 #include <functional>
 namespace lingodb::analyzer {
+#define error(message, loc)                       \
+   {                                              \
+      std::ostringstream s{};                     \
+      s << message << " at " << loc << std::endl; \
+      throw std::runtime_error(s.str());          \
+   }
+
 class Analyzer;
 class SQLQueryAnalyzer {
    public:
    SQLQueryAnalyzer(std::shared_ptr<catalog::Catalog> catalog);
    std::shared_ptr<SQLContext> context = std::make_shared<SQLContext>();
 
+   std::shared_ptr<ast::TableProducer> analyzeAndTransform(std::shared_ptr<ast::TableProducer> rootNode, std::shared_ptr<SQLContext> context);
    std::shared_ptr<ast::TableProducer> analyze(std::shared_ptr<ast::TableProducer> rootNode, std::shared_ptr<SQLContext> context);
    std::shared_ptr<ast::TableProducer> transform(std::shared_ptr<ast::TableProducer> rootNode, std::shared_ptr<ASTTransformContext> context);
+
    private:
    std::shared_ptr<catalog::Catalog> catalog;
    driver drv{};
 
-   void analyzeRootNode(std::shared_ptr<ast::AstNode> rootNode);
-
-
-   template<class T>
+   template <class T>
    std::shared_ptr<T> transformCast(std::shared_ptr<ast::TableProducer> rootNode, std::shared_ptr<ASTTransformContext> context);
 
    private:
-
-
-   void transformTargetSelections(std::shared_ptr<ast::TargetsExpression> targetSelection, std::shared_ptr<SQLContext> context);
-   void transformGroupByClause(std::shared_ptr<ast::SelectNode> rootSelectNode, std::shared_ptr<ast::GroupByNode> groupByNode, std::shared_ptr<SQLContext> context);
-   template <class T>
-   void analyzePipeOperator(std::shared_ptr<T> rootNode, std::shared_ptr<ast::PipeOperator> pipeOperator, std::shared_ptr<SQLContext> context);
-   template <class T>
-   void analyzeTargetSelections(std::shared_ptr<T> rootSelectNode, std::shared_ptr<ast::TargetsExpression> targetSelection, std::shared_ptr<SQLContext> context);
-
-   void analyzeFromClause(std::shared_ptr<ast::QueryNode> rootNode, std::shared_ptr<ast::TableRef> tableRef, std::shared_ptr<SQLContext> context);
-   void analyzeWhereClause(std::shared_ptr<ast::QueryNode> rootSelectNode, std::shared_ptr<ast::ParsedExpression> whereClause, std::shared_ptr<SQLContext> context);
-
-   void analyzeAggregation(std::shared_ptr<ast::QueryNode> rootSelectNode, std::shared_ptr<ast::AggregationNode> aggregationNode, std::shared_ptr<SQLContext> context);
-   void analyzeGroupByNode(std::shared_ptr<ast::QueryNode> rootSelectNode, std::shared_ptr<ast::GroupByNode> groupByNode, std::shared_ptr<SQLContext> context);
-
-   void analyzeModifiers(std::shared_ptr<ast::SelectNode> rootNode, std::vector<std::shared_ptr<ast::ResultModifier>> modifiers, std::shared_ptr<SQLContext> context);
-   void analyzeOrderByModifier(std::shared_ptr<ast::QueryNode> rootNode, std::shared_ptr<ast::OrderByModifier> orderByModifier, std::shared_ptr<SQLContext> context);
-   void analyzeLimitModifier(std::shared_ptr<ast::QueryNode> rootNode, std::shared_ptr<ast::LimitModifier> limitModifier, std::shared_ptr<SQLContext> context);
+   std::shared_ptr<ast::TableProducer> analyzePipeOperator(std::shared_ptr<ast::PipeOperator> pipeOperator, std::shared_ptr<SQLContext> context);
+   std::shared_ptr<ast::TableProducer> analyzeTableRef(std::shared_ptr<ast::TableRef> tableRef, std::shared_ptr<SQLContext> context);
+   std::shared_ptr<ast::BoundResultModifier> analyzeResultModifier(std::shared_ptr<ast::ResultModifier> resultModifier, std::shared_ptr<SQLContext> context);
 
    ///Expressions
-   void analyzeExpression(std::shared_ptr<ast::ParsedExpression> rootNode, std::shared_ptr<SQLContext> context);
-   void analyzeComparisonExpression(std::shared_ptr<ast::ComparisonExpression> comparison, std::shared_ptr<SQLContext> context);
-   void analyzeColumnRefExpression(std::shared_ptr<ast::ColumnRefExpression> columnRef, std::shared_ptr<SQLContext> context);
-   void analyzeStarRefExpression(const std::shared_ptr<ast::StarExpression> star, const std::shared_ptr<SQLContext> shared);
-   void analyzeConstExpression(std::shared_ptr<ast::ConstantExpression> constExpr, std::shared_ptr<SQLContext> context);
-   void analyzeConjunctionExpression(std::shared_ptr<ast::ConjunctionExpression> conjunction, std::shared_ptr<SQLContext> context);
-   void analyzeAggregationFunctionExpression(std::shared_ptr<ast::FunctionExpression> function, std::shared_ptr<SQLContext> context);
+   std::shared_ptr<ast::BoundExpression> analyzeExpression(std::shared_ptr<ast::ParsedExpression> rootNode, std::shared_ptr<SQLContext> context);
+   std::shared_ptr<ast::BoundColumnRefExpression> analyzeColumnRefExpression(std::shared_ptr<ast::ColumnRefExpression> columnRef, std::shared_ptr<SQLContext> context);
 
+   catalog::Type getCommonType(catalog::Type type1, catalog::Type type2);
+   catalog::Type getCommonBaseType(std::vector<catalog::Type> types);
    std::string createTmpScope() {
       static size_t tmpScopeCounter = 0;
       std::string scope{"tmp_attr"};
       scope.append(std::to_string(tmpScopeCounter++));
       return scope;
-   }
-
-   void error(std::string message, location loc) {
-      std::ostringstream s{};
-      s << message << " at " << loc << std::endl;
-      throw std::runtime_error(s.str());
    }
 };
 
