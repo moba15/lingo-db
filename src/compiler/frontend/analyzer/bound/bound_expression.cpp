8443dcc3ff50030a365da152570b1f35a2b752f6
Moritz Bachmaier <msb@bachmaiers.de>
2025-06-10 11:37:01 +0200

    parser, sql_analyzer: add alias support to BoundExpression classes

--- src/compiler/frontend/analyzer/bound/bound_expression.cpp
+++ src/compiler/frontend/analyzer/bound/bound_expression.cpp
@@ -3,7 +3,7 @@ namespace lingodb::ast {
 /*
  * BoundColumnRefExpression
 */
-BoundColumnRefExpression::BoundColumnRefExpression(std::string scope, catalog::NullableType resultType, std::shared_ptr<NamedResult> namedResult) : BoundExpression(TYPE, ExpressionType::BOUND_COLUMN_REF, resultType), scope(scope), namedResult(std::move(namedResult)) {
+BoundColumnRefExpression::BoundColumnRefExpression(std::string scope, catalog::NullableType resultType, std::shared_ptr<NamedResult> namedResult, std::string alias) : BoundExpression(TYPE, ExpressionType::BOUND_COLUMN_REF, resultType, alias), scope(scope), namedResult(std::move(namedResult)) {
 }
 std::string BoundColumnRefExpression::toDotGraph(uint32_t depth, NodeIdGenerator& idGen) {
    return "";
@@ -12,9 +12,9 @@ std::string BoundColumnRefExpression::toDotGraph(uint32_t depth, NodeIdGenerator
 /*
  * BoundComparisonExpression
 */
-BoundComparisonExpression::BoundComparisonExpression(ExpressionType type) : BoundExpression(TYPE, type), left(nullptr), right(nullptr) {
+BoundComparisonExpression::BoundComparisonExpression(ExpressionType type, std::string alias) : BoundExpression(TYPE, type, alias), left(nullptr), right(nullptr) {
 }
-BoundComparisonExpression::BoundComparisonExpression(ExpressionType type, std::shared_ptr<BoundExpression> left, std::shared_ptr<BoundExpression> right) : BoundExpression(TYPE, type, catalog::Type::boolean()), left(std::move(left)), right(std::move(right)) {
+BoundComparisonExpression::BoundComparisonExpression(ExpressionType type, std::string alias, std::shared_ptr<BoundExpression> left, std::shared_ptr<BoundExpression> right) : BoundExpression(TYPE, type, catalog::Type::boolean(), alias), left(std::move(left)), right(std::move(right)) {
 }
 std::string BoundComparisonExpression::toDotGraph(uint32_t depth, NodeIdGenerator& idGen) {
    return "";
@@ -23,12 +23,12 @@ std::string BoundComparisonExpression::toDotGraph(uint32_t depth, NodeIdGenerato
 /*
  * BoundConjunctionExpression
 */
-BoundConjunctionExpression::BoundConjunctionExpression(ExpressionType type, std::shared_ptr<BoundExpression> left, std::shared_ptr<BoundExpression> right) : BoundExpression(TYPE, type, catalog::Type::boolean()), children(std::vector{left, right}) {
+BoundConjunctionExpression::BoundConjunctionExpression(ExpressionType type, std::string alias, std::shared_ptr<BoundExpression> left, std::shared_ptr<BoundExpression> right) : BoundExpression(TYPE, type, catalog::Type::boolean(), alias), children(std::vector{left, right}) {
    if (type != ExpressionType::CONJUNCTION_AND && type != ExpressionType::CONJUNCTION_OR) {
       throw std::runtime_error("Invalid type for BoundConjunctionExpression");
    }
 }
-BoundConjunctionExpression::BoundConjunctionExpression(ExpressionType type, std::vector<std::shared_ptr<BoundExpression>> children) : BoundExpression(TYPE, type, catalog::Type::boolean()), children(std::move(children)) {
+BoundConjunctionExpression::BoundConjunctionExpression(ExpressionType type, std::string alias, std::vector<std::shared_ptr<BoundExpression>> children) : BoundExpression(TYPE, type, catalog::Type::boolean(), alias), children(std::move(children)) {
 }
 std::string BoundConjunctionExpression::toDotGraph(uint32_t depth, NodeIdGenerator& idGen) {
    return "";
@@ -37,7 +37,7 @@ std::string BoundConjunctionExpression::toDotGraph(uint32_t depth, NodeIdGenerat
 /*
  * BoundConstantExpression
 */
-BoundConstantExpression::BoundConstantExpression(catalog::Type resultType, std::shared_ptr<Value> value) : BoundExpression(TYPE, ExpressionType::VALUE_CONSTANT, resultType), value(std::move(value)) {
+BoundConstantExpression::BoundConstantExpression(catalog::Type resultType, std::shared_ptr<Value> value, std::string alias) : BoundExpression(TYPE, ExpressionType::VALUE_CONSTANT, resultType, alias), value(std::move(value)) {
 }
 std::string BoundConstantExpression::toDotGraph(uint32_t depth, NodeIdGenerator& idGen) {
    return "";
@@ -46,7 +46,7 @@ std::string BoundConstantExpression::toDotGraph(uint32_t depth, NodeIdGenerator&
 /*
  * BoundTargetsExpression
 */
-BoundTargetsExpression::BoundTargetsExpression(std::vector<std::shared_ptr<BoundExpression>> targets, std::vector<std::shared_ptr<NamedResult>> targetColumns) : BoundExpression(TYPE, ExpressionType::BOUND_TARGETS), targets(std::move(targets)), targetColumns(std::move(targetColumns)) {
+BoundTargetsExpression::BoundTargetsExpression(std::string alias, std::vector<std::shared_ptr<BoundExpression>> targets, std::vector<std::shared_ptr<NamedResult>> targetColumns) : BoundExpression(TYPE, ExpressionType::BOUND_TARGETS, alias), targets(std::move(targets)), targetColumns(std::move(targetColumns)) {
    for (const auto& target : this->targets) {
       if (target->type == ExpressionType::AGGREGATE && target->exprClass == ExpressionClass::FUNCTION) {
          //TODO handle aggregation
@@ -60,7 +60,7 @@ std::string BoundTargetsExpression::toDotGraph(uint32_t depth, NodeIdGenerator& 
 /*
  * BoundFunctionExpression
 */
-BoundFunctionExpression::BoundFunctionExpression(ExpressionType type, catalog::Type resultType, std::string functionName, std::string scope, std::string aliasOrUniqueIdentifier, std::vector<std::shared_ptr<BoundExpression>> arguments, std::shared_ptr<FunctionInfo> functionInfo) : BoundExpression(TYPE, type, resultType), functionName(functionName), scope(scope), aliasOrUniqueIdentifier(aliasOrUniqueIdentifier), arguments(arguments), functionInfo(functionInfo) {
+BoundFunctionExpression::BoundFunctionExpression(ExpressionType type, catalog::Type resultType, std::string functionName, std::string scope, std::string aliasOrUniqueIdentifier, std::vector<std::shared_ptr<BoundExpression>> arguments, std::shared_ptr<FunctionInfo> functionInfo) : BoundExpression(TYPE, type, resultType, aliasOrUniqueIdentifier), functionName(functionName), scope(scope), aliasOrUniqueIdentifier(aliasOrUniqueIdentifier), arguments(arguments), functionInfo(functionInfo) {
    namedResult = functionInfo;
 }
 std::string BoundFunctionExpression::toDotGraph(uint32_t depth, NodeIdGenerator& idGen) {
@@ -70,7 +70,7 @@ std::string BoundFunctionExpression::toDotGraph(uint32_t depth, NodeIdGenerator&
 /*
  * BoundStarExpression
 */
-BoundStarExpression::BoundStarExpression(std::string scope, std::vector<std::pair<std::string, catalog::Column>> columns) : BoundExpression(TYPE, ExpressionType::STAR), scope(scope), columns(std::move(columns)) {
+BoundStarExpression::BoundStarExpression(std::string scope, std::vector<std::pair<std::string, catalog::Column>> columns) : BoundExpression(TYPE, ExpressionType::STAR, ""), scope(scope), columns(std::move(columns)) {
 }
 std::string BoundStarExpression::toDotGraph(uint32_t depth, NodeIdGenerator& idGen) {
 }
@@ -78,9 +78,9 @@ std::string BoundStarExpression::toDotGraph(uint32_t depth, NodeIdGenerator& idG
 /*
  * BoundComparisonExpression
 */
-BoundOperatorExpression::BoundOperatorExpression(ExpressionType type, catalog::Type resultType, std::vector<std::shared_ptr<BoundExpression>> children) : BoundExpression(TYPE, type, resultType), children(children) {
+BoundOperatorExpression::BoundOperatorExpression(ExpressionType type, catalog::Type resultType, std::string alias, std::vector<std::shared_ptr<BoundExpression>> children) : BoundExpression(TYPE, type, resultType, alias), children(children) {
 }
-BoundOperatorExpression::BoundOperatorExpression(ExpressionType type, catalog::Type resultType, std::shared_ptr<BoundExpression> left, std::shared_ptr<BoundExpression> right) : BoundExpression(TYPE, type, resultType), children({left, right}) {
+BoundOperatorExpression::BoundOperatorExpression(ExpressionType type, catalog::Type resultType, std::string alias, std::shared_ptr<BoundExpression> left, std::shared_ptr<BoundExpression> right) : BoundExpression(TYPE, type, resultType, alias), children({left, right}) {
 }
 std::string BoundOperatorExpression::toDotGraph(uint32_t depth, NodeIdGenerator& idGen) {
    return "";
@@ -89,7 +89,7 @@ std::string BoundOperatorExpression::toDotGraph(uint32_t depth, NodeIdGenerator&
 /*
  * BoundCastExpression
 */
-BoundCastExpression::BoundCastExpression(catalog::Type resultType, std::shared_ptr<BoundExpression> child, LogicalType logicalType, std::optional<TypeMods> typeMods) : BoundExpression(TYPE, ExpressionType::CAST, resultType), child(std::move(child)), logicalType(logicalType), typeMods(typeMods) {
+BoundCastExpression::BoundCastExpression(catalog::Type resultType, std::string alias, std::shared_ptr<BoundExpression> child, LogicalType logicalType, std::optional<TypeMods> typeMods) : BoundExpression(TYPE, ExpressionType::CAST, resultType, alias), child(std::move(child)), logicalType(logicalType), typeMods(typeMods) {
 }
 std::string BoundCastExpression::toDotGraph(uint32_t depth, NodeIdGenerator& idGen) {
    return "";
@@ -98,7 +98,7 @@ std::string BoundCastExpression::toDotGraph(uint32_t depth, NodeIdGenerator& idG
 /*
  * BoundBetweenExpression
  */
-BoundBetweenExpression::BoundBetweenExpression(ExpressionType type, catalog::Type resultType, std::shared_ptr<BoundExpression> input, std::shared_ptr<BoundExpression> lower, std::shared_ptr<BoundExpression> upper) : BoundExpression(TYPE, type, resultType), input(std::move(input)), lower(std::move(lower)), upper(std::move(upper)) {
+BoundBetweenExpression::BoundBetweenExpression(ExpressionType type, catalog::Type resultType, std::string alias, std::shared_ptr<BoundExpression> input, std::shared_ptr<BoundExpression> lower, std::shared_ptr<BoundExpression> upper) : BoundExpression(TYPE, type, resultType, alias), input(std::move(input)), lower(std::move(lower)), upper(std::move(upper)) {
 }
 std::string BoundBetweenExpression::toDotGraph(uint32_t depth, NodeIdGenerator& idGen) {
    return "";
