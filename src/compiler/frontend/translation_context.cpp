02edc8d5c505b8e1c6df5fd2ecc7c0e0dd858e2b
Michael Jungmair <jungmair@in.tum.de>
2025-06-20 12:08:49 +0200

    restructure

--- src/compiler/frontend/translation_context.cpp
+++ src/compiler/frontend/translation_context.cpp
@@ -1,4 +1,4 @@
-#include "lingodb/compiler/frontend/translator/context/translation_context.h"
+#include "../../../include/lingodb/compiler/frontend/translator/context/translation_context.h"
 namespace lingodb::translator {
 
 TupleScope::TupleScope(TranslationContext* context) : context(context) {
