00f49cf26a2d59a1054957a72c0a0fee29e36b7e
Lukas Döllerer <lukas.doellerer@tum.de>
2025-11-25 14:58:54 +0100

    setup cpython-wasm

--- src/tools/cpython-wasm-test.cpp
+++ src/tools/cpython-wasm-test.cpp
@@ -10,6 +10,12 @@
 
 #include <memory>
 
+#ifndef CPYTHON_WASM_SOURCE_DIR
+#error CPYTHON_WASM_SOURCE_DIR not defined
+#endif
+
+class PyObjectRef;
+
 wasm_function_inst_t get_py_func(wasm_module_inst_t module_inst, const char* func_name) {
    wasm_function_inst_t func = wasm_runtime_lookup_function(module_inst, func_name);
    if (!func)
@@ -17,6 +23,12 @@ wasm_function_inst_t get_py_func(wasm_module_inst_t module_inst, const char* fun
    return func;
 }
 
+template <typename T>
+void pack_val(void* args_void, uint32_t& idx, T v)
+requires (std::is_same_v<T, PyObjectRef>)
+{
+   static_cast<wasm_val_t*>(args_void)[idx++].of.i32 = static_cast<int32_t>(v);
+}
 template <typename T>
 void pack_val(void* args_void, uint32_t& idx, T v)
    requires(std::integral<T> && sizeof(T) == 4)
@@ -60,94 +72,207 @@ void serialize_args(wasm_val_t* args, uint32_t& num_args, uint32_t& num_results,
    (pack_val(static_cast<void*>(args), num_args, std::forward<Ins>(ins)), ...);
 }
 
-// - Outs... must be supplied explicitly
-// - Ins... are deduced from the provided inputs.
-template <typename... Outs, typename... Ins>
-static std::vector<wasm_val_t> call_py_func(wasm_exec_env_t exec_env,
-                                            wasm_module_inst_t module_inst,
-                                            std::string_view func_name, Ins&&... ins) {
-   wasm_function_inst_t func = get_py_func(module_inst, func_name.data());
-   std::vector<wasm_val_t> args(sizeof...(Ins));
-   uint32_t num_args = 0, num_results = 0;
-   serialize_args<Outs...>(args.data(), num_args, num_results, ins...);
-   std::vector<wasm_val_t> results(num_results);
-   assert(num_args == sizeof...(Ins));
-   bool success = wasm_runtime_call_wasm_a(exec_env, func, num_results, results.data(), num_args, args.data());
-   if (!success) {
-      /* exception is thrown if call fails */
-      throw std::runtime_error{wasm_runtime_get_exception(module_inst)};
+class WasmRuntime {
+public:
+   WasmRuntime() {
+      wasm_runtime_init();
    }
-   return results;
+   ~WasmRuntime() {
+      wasm_runtime_destroy();
+   }
+};
+
+class CPythonModule {
+public:
+   CPythonModule(std::unique_ptr<WasmRuntime> runtime) : runtime{std::move(runtime)} {
+      /* read WASM file into a memory buffer */
+      uint32_t bufSize;
+      uint8_t* buffer = reinterpret_cast<uint8_t*>(bh_read_file_to_buffer(CPYTHON_WASM_AOT_FILE, &bufSize));
+      if (!buffer) {
+         throw std::runtime_error{"Failed to read WASM file"};
+      }
+
+      /* parse the WASM file from buffer and create a WASM module */
+      char errorBuf[128] = {0};
+      module = wasm_runtime_load(buffer, bufSize, errorBuf, sizeof(errorBuf));
+      if (!module) {
+         throw std::runtime_error{std::format("Failed to load WASM module: {}", errorBuf)};
+      }
+   }
+
+   ~CPythonModule() {
+      wasm_runtime_unload(module);
+   }
+
+   wasm_module_t module;
+
+private:
+   std::unique_ptr<WasmRuntime> runtime;
+};
+
+class CPythonInst {
+public:
+   constexpr static uint32 stackSize = 1677721600; // 16MB
+   constexpr static uint32 heapSize = 8092;    // 8KB
+
+   CPythonInst(std::unique_ptr<CPythonModule> cpythonModule) : cpythonModule{std::move(cpythonModule)} {
+      char errorBuf[128] = {0};
+
+      /* --- 1. Setup 'dir_list' (for --dir=.) --- */
+      // This sets the CWD for python.aot (This is correct)
+      std::array dirList{
+         CPYTHON_WASM_BUILD_DIR};
+
+      /* --- 2. Setup 'map_dir_list' (for --map-dir=...) --- */
+      std::array mapDirList{
+         "/::" CPYTHON_WASM_SOURCE_DIR,
+         "/lib/python3.14::" CPYTHON_WASM_SOURCE_DIR "/Lib",
+      };
+
+      // spoof fake program name (since CPython uses it to load relativ libs)
+      std::string programName = "./python.aot";
+      std::array wasmArgs{programName.data()};
+
+      wasm_runtime_set_wasi_args(this->cpythonModule->module,
+                                 dirList.data(), dirList.size(),
+                                 mapDirList.data(), mapDirList.size(),
+                                 nullptr, 0,
+                                 wasmArgs.data(), wasmArgs.size());
+      moduleInst = wasm_runtime_instantiate(this->cpythonModule->module, stackSize, heapSize,
+                                                               errorBuf, sizeof(errorBuf));
+      if (!moduleInst) {
+         throw std::runtime_error{std::format("Failed to instantiate CPython module: {}", errorBuf)};
+      }
+   }
+
+   ~CPythonInst() {
+      wasm_runtime_deinstantiate(moduleInst);
+   }
+
+   wasm_module_inst_t moduleInst;
+private:
+     std::unique_ptr<CPythonModule> cpythonModule;
+};
+
+class PyEnv {
+public:
+   // - Outs... must be supplied explicitly
+   // - Ins... are deduced from the provided inputs.
+   template <typename... Outs, typename... Ins>
+   std::vector<wasm_val_t> callPyFunc(const std::string& funcName, Ins&&... ins) {
+      wasm_function_inst_t func = get_py_func(moduleInst, funcName.c_str());
+      std::vector<wasm_val_t> args(sizeof...(Ins));
+      uint32_t nameArgs = 0, numResults = 0;
+      serialize_args<Outs...>(args.data(), nameArgs, numResults, ins...);
+      std::vector<wasm_val_t> results(numResults);
+      assert(nameArgs == sizeof...(Ins));
+      bool success = wasm_runtime_call_wasm_a(execEnv, func, numResults, results.data(), nameArgs, args.data());
+      if (!success) {
+         /* exception is thrown if call fails */
+         throw std::runtime_error{wasm_runtime_get_exception(moduleInst)};
+      }
+      return results;
+   }
+
+   PyEnv(std::unique_ptr<CPythonInst> cpythonInst) : moduleInst{cpythonInst->moduleInst}, cpythonInst{std::move(cpythonInst)} {
+      execEnv = wasm_runtime_create_exec_env(this->cpythonInst->moduleInst, this->cpythonInst->stackSize);
+      if (!execEnv) {
+         throw std::runtime_error{"Failed to create execution environment"};
+      }
+      callPyFunc<void>("Py_Initialize");
+   }
+
+   ~PyEnv() {
+      wasm_runtime_destroy_exec_env(execEnv);
+   }
+
+   void testSetup() {
+      const auto init = callPyFunc<bool>("Py_IsInitialized").at(0).of.i32;
+      if (!init) {
+         throw std::runtime_error{std::format("Py_Initialize failed")};
+      }
+      std::cout << "Python initialized successfully" << std::endl;
+   }
+
+   wasm_exec_env_t execEnv;
+
+   // for conveniance
+   wasm_module_inst_t moduleInst;
+private:
+   std::unique_ptr<CPythonInst> cpythonInst;
+};
+
+class PyObjectRef {
+   public:
+   PyObjectRef(const int32_t ptr, std::shared_ptr<PyEnv> pyEnv) : ptr{ptr}, pyEnv{pyEnv} {}
+   ~PyObjectRef() {
+      pyEnv->callPyFunc<void>("Py_DecRef", ptr);
+   }
+
+   operator uint32_t() const { return ptr; }
+
+   private:
+   int32_t ptr;
+   std::shared_ptr<PyEnv> pyEnv;
+};
+
+static PyObjectRef importModule(const std::string& moduleName, std::shared_ptr<PyEnv> pyEnv) {
+   void* nativeBuf = nullptr;
+   uint64_t wasmBuf = wasm_runtime_module_malloc(pyEnv->moduleInst, 200, &nativeBuf);
+   auto* nativeCharBuf = std::bit_cast<char*>(nativeBuf);
+
+   memcpy(nativeCharBuf, moduleName.c_str(), moduleName.size() + 1);
+   // pName = PyUnicode_DecodeFSDefault(moduleName);
+   auto pName =  pyEnv->callPyFunc<PyObjectRef>("PyUnicode_DecodeFSDefault", wasmBuf).at(0).of.i32;
+   //pModule = PyImport_Import(pName);
+   auto pModule =  pyEnv->callPyFunc<PyObjectRef>("PyImport_Import", pName).at(0).of.i32;
+   if (!pModule) {
+      pyEnv->callPyFunc<void>("PyErr_Print");
+      throw std::runtime_error{"Module not found"};
+   }
+   return {pModule, pyEnv};
 }
 
 int main(int argc, char** argv) {
-   char error_buf[128] = {0};
-   uint32 size, stack_size = 8092, heap_size = 8092;
+   std::unique_ptr<WasmRuntime> runtime = std::make_unique<WasmRuntime>();
+   std::unique_ptr<CPythonModule> cpythonModule = std::make_unique<CPythonModule>(std::move(runtime));
+   std::unique_ptr<CPythonInst> cpythonInst = std::make_unique<CPythonInst>(std::move(cpythonModule));
+   std::shared_ptr<PyEnv> pyEnv = std::make_shared<PyEnv>(std::move(cpythonInst));
+   pyEnv->testSetup();
 
-   /* initialize the wasm runtime by default configurations */
-   wasm_runtime_init();
+   {
+      PyObjectRef osModule = importModule(std::string{"os"}, pyEnv);
+      const char* fName = "getcwd";
+      // Malloc buffer in wasm for the function
+      void* nativeBufAddr = nullptr;
+      uint64_t fNameWasmAddr = wasm_runtime_module_malloc(pyEnv->moduleInst, strlen(fName) + 1, &nativeBufAddr);
+      if (!nativeBufAddr) {
+         throw std::runtime_error{"Failed to malloc wasm buffer for function name"};
+      }
+      char* nativeCharBuf = std::bit_cast<char*>(nativeBufAddr);
+      // Copy function name into wasm buffer
+      memcpy(nativeCharBuf, fName, strlen(fName) + 1);
 
-   /* read WASM file into a memory buffer */
-   uint8_t* buffer = reinterpret_cast<uint8_t*>(bh_read_file_to_buffer("/home/bachmaier/projects/lingo-db/build/cpython-wasm/Python-3.14.0/cross-build/wasm32-wasip1/python.aot", &size));
-   if (!buffer) {
-      std::cerr << "Failed to read WASM file" << std::endl;
-      return -1;
+      PyObjectRef pFunc = {pyEnv->callPyFunc<uint32_t>("PyObject_GetAttrString", osModule, fNameWasmAddr).at(0).of.i32, pyEnv};
+      //Check callable
+      std::cerr << "pFunc: " << pFunc << std::endl;
+      if (!pFunc || pyEnv->callPyFunc<bool>("PyCallable_Check", pFunc).at(0).of.i32 == 0) {
+         throw std::runtime_error{"Function is not callable"};
+      }
+
+      // call function
+      PyObjectRef resultObj = {pyEnv->callPyFunc<uint32_t>("PyObject_CallObject", pFunc, 0).at(0).of.i32, pyEnv};
+      if (!resultObj) {
+         throw std::runtime_error{"Failed to call function"};
+      }
+      // Convert result to string
+      auto resultStrObj = pyEnv->callPyFunc<PyObjectRef>("PyObject_Str", resultObj).at(0).of.i32;
+      if (!resultStrObj) {
+         throw std::runtime_error{"Failed to convert result to string"};
+      }
+      // Convert string object to C string
+      auto cStrWasmAddr = pyEnv->callPyFunc<uint32_t>("PyUnicode_AsUTF8", resultStrObj).at(0).of.i32;
+      void* nativecStr = wasm_runtime_addr_app_to_native(pyEnv->moduleInst, cStrWasmAddr);
+      std::cout << std::bit_cast<char*>(nativecStr) << std::endl;
    }
-
-   /* parse the WASM file from buffer and create a WASM module */
-   wasm_module_t module = wasm_runtime_load(buffer, size, error_buf, sizeof(error_buf));
-   if (!module) {
-      std::cerr << error_buf << std::endl;
-      return -1;
-   }
-
-   /* create an instance of the WASM module (WASM linear memory is ready) */
-   wasm_module_inst_t module_inst = wasm_runtime_instantiate(module, stack_size, heap_size,
-                                                             error_buf, sizeof(error_buf));
-   if (!module_inst) {
-      std::cerr << error_buf << std::endl;
-      wasm_runtime_unload(module);
-      return -1;
-   }
-
-   wasm_exec_env_t exec_env = wasm_runtime_create_exec_env(module_inst, stack_size);
-   if (!exec_env) {
-      std::cerr << "Create exec env failed" << std::endl;
-      return -1;
-   }
-
-   // test if interpreter is initialized
-   auto results = call_py_func<char*>(exec_env, module_inst, "Py_GetVersion");
-   std::string python_version{std::bit_cast<char*>(wasm_runtime_addr_app_to_native(module_inst, results[0].of.i32))};
-   std::cout << std::format("Python version: {}\n", python_version);
-
-   // hello world
-   using PyObjectPtr = uint32_t;
-   void* native_buf_addr = nullptr;
-   uint64_t inst_buf_addr = wasm_runtime_module_malloc(module_inst, 200, &native_buf_addr);
-
-   std::string part1{"Hello "};
-   std::string part2{"World!"};
-
-   char* native_char_buf = std::bit_cast<char*>(native_buf_addr);
-   memcpy(native_char_buf, part1.c_str(), part1.size() + 1);
-   uint32_t wasm_part1_str = static_cast<uint32_t>(inst_buf_addr);
-   memcpy(native_char_buf + part1.size() + 1, part2.c_str(), part2.size() + 1);
-   uint32_t wasm_part2_str = static_cast<uint32_t>(inst_buf_addr + part1.size() + 1);
-
-   PyObjectPtr part1_obj = call_py_func<PyObjectPtr>(exec_env, module_inst, "PyUnicode_FromString", wasm_part1_str)[0].of.i32;
-   PyObjectPtr part2_obj = call_py_func<PyObjectPtr>(exec_env, module_inst, "PyUnicode_FromString", wasm_part2_str)[0].of.i32;
-   PyObjectPtr joined_obj = call_py_func<PyObjectPtr>(exec_env, module_inst, "PyUnicode_Concat", part1_obj, part2_obj)[0].of.i32;
-   call_py_func<void>(exec_env, module_inst, "Py_DecRef", part1_obj);
-   call_py_func<void>(exec_env, module_inst, "Py_DecRef", part2_obj);
-   uint32_t wasm_result_str = call_py_func<char*>(exec_env, module_inst, "PyUnicode_AsUTF8", joined_obj)[0].of.i32;
-   std::string result_str{std::bit_cast<char*>(wasm_runtime_addr_app_to_native(module_inst, wasm_result_str))};
-   std::cout << std::format("Result string: {}\n", result_str);
-   call_py_func<void>(exec_env, module_inst, "Py_DecRef", joined_obj);
-
-   // free all
-   wasm_runtime_destroy_exec_env(exec_env);
-   wasm_runtime_deinstantiate(module_inst);
-   wasm_runtime_unload(module);
-   wasm_runtime_destroy();
 }
